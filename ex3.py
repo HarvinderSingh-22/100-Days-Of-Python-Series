dfc1c5b730e89a00a46708194c831938983a0936
Harvinder Singh Rajput <harvinder.sr22@gmail.com>
2025-05-31 00:09:32 +0530

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,11 @@
+# this code file is interconnected with lec3 file
+
+
+def greeting(name):                            # Creating a greeting function
+  print("Hello, " + name)
+
+dict2 = {                                     # Creating a dictionary contains a key value pairs
+  "name": "Gul",
+  "age": 22,
+  "country": "India"
+}
